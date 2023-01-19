8101a9b45481e7a3637d9b4067ce1a79afde2bba
palaksharma23 <mahimasharma2312@gmail.com>
2023-01-20 00:12:20 +0530

    Initial Files

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { AppBar, Avatar, Toolbar, Typography, Button } from "@material-ui/core";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import useStyles from "./styles";
+import snaps from "../../images/logo.png";
+import snaptext from "../../images/logotext.png";
+import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
+
+const Navbar = () => {
+  const classes = useStyles();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    navigate("/");
+  };
+
+  useEffect(() => {
+    const token = user?.token;
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) {
+        logout();
+      }
+    }
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
+
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <Link to="/" className={classes.brandContainer}>
+        {/* <img src={snaptext} alt="SnapIcon" height="45px" /> */}
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: { xs: "none", md: "flex" },
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          SnapUp
+        </Typography>
+        <img className={classes.image} src={snaps} alt="icon" height="40" />
+      </Link>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.picture}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.username} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+              onClick={logout}
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
